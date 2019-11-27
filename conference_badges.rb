a856a098348e117da02e9ae5d91e9b4c71b280fe
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-11-27 22:54:00 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -21,7 +21,12 @@ def assign_rooms(array)
   return roomarray
 end
 
-def printer
-  print batch_badge_creator
-  print assign_rooms
+def printer (array)
+  batch_badge_creator(array).each do |id|
+    puts id
+  end 
+  
+  assign_rooms(array).each do |id|
+    puts id 
+  end
 end
