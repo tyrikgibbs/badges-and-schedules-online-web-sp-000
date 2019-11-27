b3bc04bbe5c47b5531e383012be9d7bb673f1966
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-11-27 22:23:23 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,5 +3,13 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
+def batch_badge_creator (array)
+  badgearray = []
+  array.each do |name|
+    badgearray.push ("Hellow, my name is #{name}.")
+  end
+  return badgearray
+end
+
 def assign_rooms
 end
